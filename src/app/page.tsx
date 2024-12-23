5b89415b41ba9b776677ad8062e8046f528c3169
silvershade1 <silvershade1000@gmail.com>
2024-12-23 23:03:47 +0330

    customize components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button size="sm">click</Button>
-      <p className="text-green-500">hello</p>
+    <div className="flex gap-4">
+      <Input />
+      <Button>primary</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="muted">muted</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="teritary">outline</Button>
     </div>
   );
 }
