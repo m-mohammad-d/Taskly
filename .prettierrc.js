a110a984df6119be907deb777c292880f5a1c67a
silvershade1 <silvershade1000@gmail.com>
2024-12-24 14:11:16 +0330

    config prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 200,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
