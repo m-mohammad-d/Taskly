3739377e7b4b0a873945cfc87aaede66d598290a
silvershade1 <silvershade1000@gmail.com>
2025-01-01 13:36:09 +0330

    feat(auth): add redirect to login for CreateWorkspace and Workspace pages when not authenticated

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,11 @@
-function page() {
+import { getCurrent } from "@/features/auth/action";
+import { redirect } from "next/navigation";
+
+async function page() {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
   return <div></div>;
 }
 
--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,6 +1,12 @@
+import { getCurrent } from "@/features/auth/action";
 import CreateWorkspaceForm from "@/features/workSpaces/components/CreateWorkspaceForm";
+import { redirect } from "next/navigation";
 
-function page() {
+async function page() {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
   return (
     <div className="w-full lg:max-w-xl">
       <CreateWorkspaceForm />
